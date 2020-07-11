1bb2d2de1e7c23d6a6cd88457dd086b3b4430eba
pierreGarciaRaf <56490823+pierreGarciaRaf@users.noreply.github.com>
2020-07-11 12:35:55 +0200

    no more mouse getting out of the screen

--- src/world/player/Cursor.gd
+++ src/world/player/Cursor.gd
@@ -2,21 +2,25 @@ extends KinematicBody2D
 
 
 
+var mouseMoveBetweenProcess =  Vector2.ZERO
 var velocity = Vector2()
 
 var lastMousePos = Vector2(0,0)
 
+func _input(event):
+	#so the mouse is captured
+	if event is InputEventMouseMotion:
+		mouseMoveBetweenProcess += (event as InputEventMouseMotion).relative
+
 func get_input():
-	#removes the tp bug
-	var newMousePos = get_viewport().get_mouse_position()
-	velocity = newMousePos - lastMousePos
-	lastMousePos = newMousePos
+	#removes the tp bug + allows the mouse to move while it's captured
+	velocity = mouseMoveBetweenProcess
+	mouseMoveBetweenProcess = Vector2.ZERO
 
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide(velocity/delta)
-
 	if get_slide_count()>0 :
 		#print (collision, collision.collider)
 		$Sprite.modulate = Color(1,0,0,1)
