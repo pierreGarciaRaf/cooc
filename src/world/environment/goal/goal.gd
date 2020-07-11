5b7a9da18734f7f2169eea5314fd541166add491
pierreGarciaRaf <56490823+pierreGarciaRaf@users.noreply.github.com>
2020-07-11 19:17:15 +0200

    import recuperer

--- src/world/environment/goal/goal.gd
+++ src/world/environment/goal/goal.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal end_level
+var bodyInside = false
 
 func _ready():
 	pass # Replace with function body.
@@ -10,5 +11,13 @@ func _ready():
 func _on_goal_body_entered(body):
 	print(body.name)
 	if body.is_in_group("player"):
-		print("player in")
+		$AnimatedSprite.play("hovered")
+		
+
+func _input(event):
+	if bodyInside and event.type is InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed():
+		$AnimatedSprite.play("checked")
 		emit_signal("end_level")
+		
+
+
