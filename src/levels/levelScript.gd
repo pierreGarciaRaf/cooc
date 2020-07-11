eb7754dd68606032c8f6e9834031b01136876beb
pierreGarciaRaf <56490823+pierreGarciaRaf@users.noreply.github.com>
2020-07-11 12:27:53 +0200

    reorganisation

--- src/levels/levelScript.gd
+++ src/levels/levelScript.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
 
 
 func _on_Cursor_body_entered(body):
 	print('body')
-
-
 
